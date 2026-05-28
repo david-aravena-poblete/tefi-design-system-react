0c894c0391ff7d99e1612147ca4d16b8d3b9dd78
david.aravena.pob <david.aravena.pob@gmail.com>
2026-05-28 13:33:47 +0000

    release: prepare library v0.2.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,26 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [],
-  "framework": "@storybook/react-vite"
+
+  addons: [],
+
+  framework: "@storybook/react-vite",
+
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      server: {
+        host: "0.0.0.0",
+
+        hmr: {
+          protocol: "wss",
+        },
+      },
+    });
+  },
 };
+
 export default config;
